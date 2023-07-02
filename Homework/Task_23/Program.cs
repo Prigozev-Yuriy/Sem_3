7aa7d50c7aff97c1c2daff59dc3a651b91cb473a
Prigozev-Yuriy <138002979+Prigozev-Yuriy@users.noreply.github.com>
2023-07-03 00:37:49 +0300

    CommitTask_23

---
+++ Homework/Task_23/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите число: ");
+int end = Convert.ToInt32(Console.ReadLine());
+if (end > 0)
+{
+    for (int i = 1; i <= end; i++)
+    {
+        Console.Write(Math.Pow(i, 3) + " ");
+    }
+}
+else
+{
+    for (int i = 1; i >= end; i--)
+    {
+        Console.Write(Math.Pow(i, 3) + " ");
+    }
+}
